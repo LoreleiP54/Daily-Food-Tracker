ea4049c501bc2bb6aa05fc340523c4913864326d
LoreleiP54 <LoreleiPotter54@gmail.com>
2026-04-21 13:43:50 -0500

    Created food item model class for food addition in the app

---
+++ lib/models/food_item.dart
@@ -0,0 +1,15 @@
+class FoodItem {
+  final String name;
+  final double caloriesPerServing;
+  final double proteinPerServing;
+  final double carbsPerServing;
+  final double fatPerServing;
+
+  FoodItem({
+    required this.name,
+    required this.caloriesPerServing,
+    required this.proteinPerServing,
+    required this.carbsPerServing,
+    required this.fatPerServing,
+  });
+}
